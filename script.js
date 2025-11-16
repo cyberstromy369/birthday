7df5bff811c117c8b3b48d6e2de0bf7d2c50e8b8
cyberstromy369 <shamitmondal880@gmail.com>
2025-11-16 18:51:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updateCountdown() {
       const now = new Date();
-      const birthday = new Date("2025-11-26T00:00:00");
+      const birthday = new Date("2026-03-28T00:00:00");
 
       let timeDiff = birthday - now;
 
@@ -96,3 +96,4 @@ modeToggle.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
 });
+
